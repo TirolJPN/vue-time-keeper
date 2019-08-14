6673af42b5bddcd299e03117fae9dedd57c6e549
Tirol_JPN <Tirol.JPN@gmail.com>
2019-08-14 23:18:53 +0900

    add script to log google sheet | close #2

--- src/components/displayInfoOfSpreadSheet.vue
+++ src/components/displayInfoOfSpreadSheet.vue
@@ -17,7 +17,7 @@ export default {
         return {
             clientId: process.env.VUE_APP_CLIENT_ID,
             apiKey: process.env.VUE_APP_API_KEY,
-
+            sheetId: process.env.VUE_APP_SHEET_ID,
         };
     },
     mounted: function() {
@@ -27,15 +27,29 @@ export default {
     },
     methods: {
         clientInitialize() {
+            const self = this
             window.gapi.load('client:auth2', ()=> {
+                // authの初期化
                 gapi.client.init({
                     clientId: this.clientId,
                     scope: 'https://www.googleapis.com/auth/spreadsheets',
                     discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4']
                 });
+                // GoogleAuthオブジェクトを取得
+                // ref: https://developers.google.com/identity/sign-in/web/reference
                 const auth = window.gapi.auth2.getAuthInstance();
                 auth.signIn();
+                console.log('is logined?: ' + auth.isSignedIn.get())
+
+                // シートデータの取得
+                gapi.client.sheets.spreadsheets.values.get({
+                    spreadsheetId: self.sheetId,
+                    range: 'time-keeper!A1:F13'
+                }).then((res)=>{
+                    console.log(res)
+                });
             });
+            
         }
     }
 }
