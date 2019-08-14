1d1a99d6cf1087753ec13c443b00966b3ade7a27
Tirol_JPN <Tirol.JPN@gmail.com>
2019-08-14 21:54:20 +0900

    add auth func

--- src/components/displayInfoOfSpreadSheet.vue
+++ src/components/displayInfoOfSpreadSheet.vue
@@ -1,13 +1,42 @@
 <template>
     <div>
+        <hr>
         <h1>
-            hoge
+            title
         </h1>
+        <h2>{{clientId}}</h2>
+        <h2>{{apiKey}}</h2>
+        <button @click="clientInitialize">alert</button>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'displayInfoOfSpreadSheet'
+    name: 'displayInfoOfSpreadSheet',
+    data () {
+        return {
+            clientId: process.env.VUE_APP_CLIENT_ID,
+            apiKey: process.env.VUE_APP_API_KEY,
+
+        };
+    },
+    mounted: function() {
+        // for debug
+        console.log(this.clientId)
+        console.log(this.apiKey)
+    },
+    methods: {
+        clientInitialize() {
+            window.gapi.load('client:auth2', ()=> {
+                gapi.client.init({
+                    clientId: this.clientId,
+                    scope: 'https://www.googleapis.com/auth/spreadsheets',
+                    discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4']
+                });
+                const auth = window.gapi.auth2.getAuthInstance();
+                auth.signIn();
+            });
+        }
+    }
 }
 </script>
