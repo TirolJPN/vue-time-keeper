5cafd8c1026c16c6e6ac3a43167370e99c0b2312
Tirol_JPN <Tirol.JPN@gmail.com>
2019-09-07 09:52:18 +0900

    install gapu types and fix undefined type errors on displayInfoOfSpreadSheet.vue

--- src/components/displayInfoOfSpreadSheet.vue
+++ src/components/displayInfoOfSpreadSheet.vue
@@ -30,27 +30,27 @@ export default class displayInfoOfSpreadSheet extends Vue {
     /** methods **/
     clientInitialize() {
         const self = this;
-            window.gapi.load('client:auth2', ()=> {
+            (<any>window).gapi.load('client:auth2', ()=> {
             // authの初期化
-            window.gapi.client.init({
+            (<any>window).gapi.client.init({
                 clientId: self.clientId,
                 scope: 'https://www.googleapis.com/auth/spreadsheets',
                 discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4']
             }).then(function () {
                 // GoogleAuthオブジェクトを取得
                 // ref: https://developers.google.com/identity/sign-in/web/reference
-                const auth = window.gapi.auth2.getAuthInstance();
+                const auth = (<any>window).gapi.auth2.getAuthInstance();
                 // ポップアップが閉じられると同時に状態を変更
                 auth.signIn().then( () => 
                     self.isLogined = auth.isSignedIn.get()
                 )
             });
         });
-    };
+    }
 
     logOut () {
         const self = this;
-        const auth = window.gapi.auth2.getAuthInstance();
+        const auth = (<any>window).gapi.auth2.getAuthInstance();
         // 既にログイン状態ならば
         if(auth.isSignedIn.get()){
             auth.signOut().then( function() {
@@ -58,16 +58,16 @@ export default class displayInfoOfSpreadSheet extends Vue {
                 self.isLogined = auth.isSignedIn.get();
             })
         }
-    };
+    }
 
     fetchSpreadSheetData() {
         // シートデータの取得
-        window.gapi.client.sheets.spreadsheets.values.get({
+        (<any>window).gapi.client.sheets.spreadsheets.values.get({
             spreadsheetId: this.sheetId,
             range: 'time-keeper!A1:F13'
-        }).then((res)=>{
+        }).then((res: any)=>{
             this.data = res.result.values;
         });
-    };
+    }
 }
 </script>
